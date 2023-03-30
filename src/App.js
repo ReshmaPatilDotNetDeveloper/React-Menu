f0be5124f16b3ce46fa092e6cab8ddd6518ce4d4
Reshma Patil <reshmapatildotnetdeveloper@gmail.com>
2023-03-30 14:40:45 +0530

    Added react menu project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import menu from './data';
+import React,{ useState } from 'react'
+import MenuComponent from './menu';
+import Category from './Category';
+
+let categories = ['all',...new Set(menu.map(item => item.category))];
+console.log(categories);
 
 function App() {
+  const[menuItems,setMenuItems] = useState(menu);
+  const filterItems = (category) =>{
+    if(category == 'all'){
+      setMenuItems(menu);
+    }
+    else{
+      let items = menu.filter(item => item.category == category);
+      setMenuItems(items);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <section className="menu section">
+      <div className="title">
+        <h2>our menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Category categories={categories} filterItems={filterItems}/>
+      <MenuComponent items={menuItems}/>
+    </section >
+  </main>
   );
 }
 
